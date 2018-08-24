9280464abc3765f284c4453ea79b9426f5c47e15
Ian Zunderdorp <ian.zunderdorp@mobiliteitsfabriek.nl>
2018-08-24 12:28:38 +0200

    Static chat

---
+++ widget/src/Slack/SlackMessageInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Textarea from "./Textarea";
+import Submit from "./Submit";
+
+export default () => (
+  <div className="input">
+    <Textarea />
+    <Submit>Send</Submit>
+  </div>
+);
