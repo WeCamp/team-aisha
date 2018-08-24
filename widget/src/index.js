af77e4500728ef3d9b58f4fdfad1df2b638a07e1
Jasper Mutsaerts <jaspermutsaerts@gmail.com>
2018-08-24 11:34:36 +0200

    Voorbeeldje slack message posten via websocket

--- widget/src/index.js
+++ widget/src/index.js
@@ -1,20 +1,82 @@
-import "babel-polyfill";
+class SlackClient extends React.Component {
 
-import { ReactSlackChat } from "react-slack-chat";
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: '',
+      websocket: null
+    }
+  }
+
+  onSlackEvent(event) {
+    console.log('a slack event happened!', JSON.parse(event.data));
+  }
+
+  sendMessage() {
+    if (this.state.message === '') {
+      console.error('empty message');
+      return;
+    }
+
+    if (this.state.websocket === null) {
+      console.error('no open websocket')
+      return
+    }
+
+    this.state.websocket.send(JSON.stringify({
+      "type":"message",
+      "text":this.state.message,
+      "channel":"CCBPV1E9F"}
+    ));
+  }
+
+  changeMessage(event) {
+    this.setState({
+      message: event.target.value
+    });
+  }
+
+  componentDidMount() {
+    console.log('mounted')
+    var initUrl = "https://slack.com/api/rtm.connect?token=xoxb-3547151076-420352797363-jQbOTPcBCWuR2jaeaSKpQ883";
+    fetch(initUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      credentials: "same-origin"
+    }).then(function(response) {
+      return response.json();
+    }).then(body => {
+      var websocket = new WebSocket(body.url)
+      this.setState({websocket: websocket})
+      websocket.onmessage = this.onSlackEvent
+    }).catch(console.error)
+  }
+
+  componentWillUnmount() {
+    if (this.state.websocket !== null) {
+      console.log('close connection')
+      this.state.websocket.close()
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      <textarea onChange={e => this.changeMessage(e)}></textarea>
+      <button onClick={e => this.sendMessage()}>Post</button>
+      </div>
+    );
+  }
+}
+
 
 class Component extends React.Component {
   render() {
     return (
       <div className="react-slack-chat">
-        <ReactSlackChat
-          botName="reactor-the-bot"
-          apiToken={btoa(
-            "xoxb-3547151076-420352797363-jQbOTPcBCWuR2jaeaSKpQ883"
-          )}
-          channels={[{ id: "T03G34F28", name: "reactor" }]}
-          userImage="http://www.iconshock.com/img_vista/FLAT/mail/jpg/robot_icon.jpg"
-          debugMode
-        />
+        <SlackClient />
       </div>
     );
   }
